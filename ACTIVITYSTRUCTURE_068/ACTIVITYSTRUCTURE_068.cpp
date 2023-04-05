19a327013672088e5aa6490b58c2fd83eca36501
Jantopthegreat <fauzangriya@gmail.com>
2023-04-05 11:29:08 +0700

    membuat input untuk NIM, Umur, Nama, Desa, Kota

--- ACTIVITYSTRUCTURE_068/ACTIVITYSTRUCTURE_068.cpp
+++ ACTIVITYSTRUCTURE_068/ACTIVITYSTRUCTURE_068.cpp
@@ -6,15 +6,37 @@ struct mahasiswa
 	string nim;
 	string nama;
 	string alamat;
-	int umur
-};
+	int umur;
+};  
 
 
 int main() {
 	mahasiswa mhs, mhs2;
 
-	mhs.nim = "20210140019";
-	mhs.umur = 20;
-	mhs.nama = "Ardian Mulya";
+	mhs.nim = "20220140068";
+	mhs.umur = 19;
+	mhs.nama = "Fauzan Althaf Rianto";
+	mhs.alamat = "Bandung";
+
+	cout << "masukan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+	cout << "Masukan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat : ";
+	cin >> mhs2.alamat;
+
+	cout << "NIM :" << mhs.nim;
+	cout << "\nUmur : " << mhs.umur;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nAlamat : " << mhs.alamat;
+
+	cout << "\n\nNIM : " << mhs2.nim;
+	cout << "\n\nUmur : " << mhs2.umur;
+	cout << "\n\nNama : " << mhs2.nama;
+	cout << "\n\nAlamat : " << mhs2.alamat;
+
+
 
 }
---
+++ latihan3/latihan3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "masukan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukan Umur : ";
+		cin.getline(mhs[i].nama, 20);
+		cin.ignore(1, '\n');
+		cout << "Masukan Nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tmasukan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+}
+
+	for (int i= 0; i < 3; i++)
