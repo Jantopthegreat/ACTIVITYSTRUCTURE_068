7c53c12abfc0ea5c4665f5d1e669b6c2c6c439d2
Jantopthegreat <fauzangriya@gmail.com>
2023-04-05 10:27:51 +0700

    membuat struktur mahasiwa

---
+++ ACTIVITYSTRUCTURE_068/ACTIVITYSTRUCTURE_068.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+	string nim;
+	string nama;
+	string alamat;
+	int umur
+};
+
+
+int main() {
+	mahasiswa mhs, mhs2;
+
+	mhs.nim = "20210140019";
+	mhs.umur = 20;
+	mhs.nama = "Ardian Mulya";
+
+}
